05007f4b2e2d326f913acbc76cc41076aeb3226d
Nalin R Trivedi <nishu.tri666@gmail.com>
2022-01-06 15:27:33 +0530

    add start script

--- app.js
+++ app.js
@@ -92,6 +92,7 @@ app.all('*', (req, res, next) => {
     next(new ExpressError("Page not found!", 404));
 })
 
-app.listen(3000, () => {
-    console.log('Serving to port 3000');
+const port=process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving to port ${port}`);
 })
