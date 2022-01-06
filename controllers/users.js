43403de1d57b13374b10a244f415649fc70447c4
Nalin R Trivedi <nishu.tri666@gmail.com>
2022-01-06 17:27:30 +0530

    final 1

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 module.exports.renderNewUser = (req, res) => {
     res.render('users/register');
 };
-module.exports.createNewUser = async (req, res) => {
+module.exports.createNewUser = async (req, res,next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
@@ -13,9 +13,9 @@ module.exports.createNewUser = async (req, res) => {
             req.flash('success', 'Welcome');
             res.redirect('/campgrounds');
         })
+        next();
     } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
+        next(e);
     }
 };
 module.exports.renderLogin = (req, res) => {
